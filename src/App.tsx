afde369f4de585f862372f100236aa6d36e53e10
mateus weinert <mateuswhj@gmail.com>
2025-10-21 13:26:41 -0300

    add backgorund

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
 
 import './App.css'
-import { Box } from '@mui/material'
+import { Box, createTheme, Divider, ThemeProvider, Typography } from '@mui/material'
+
+const theme = createTheme();
+
+theme.typography.h3 = {
+  fontSize: '3.2rem',
+  '@media (min-width:600px)': {
+    fontSize: '3.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '4rem',
+  },
+};
 
 function App() {
 
   return (
     <>
-      <InfoBox></InfoBox>
+      <Header></Header>
+      <Divider sx={{ height: "2px", backgroundColor: "white" }}></Divider>
+      <Box sx={{
+        position: 'relative',
+        width: '100%', height: 1000,
+        backgroundImage: `url('src/assets/ocean_background.jpg')`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
+        <InfoBox></InfoBox>
+      </Box>
+
     </>
   )
+}
+
+export function Header() {
+  return (
+    <>
+      <Box sx={{
+        width: '100%',
+        paddingLeft: 0,
+        paddingRight: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        display: 'flex',
+        justifyContent: 'center',
+      }} >
+        <ThemeProvider theme={theme}>
+          <Typography variant="h3">BIO NEWS</Typography>
+        </ThemeProvider>
+      </Box>
+    </>
+  );
 }
 
 export function InfoBox() {
@@ -36,9 +83,8 @@ export function Tentacle1() {
     style={{
       position: 'absolute',
       top: 345,
-      left: 510,
+      left: 509,
       zIndex: 1,
-      rotate: '90deg',
     }} />;
 }
 
