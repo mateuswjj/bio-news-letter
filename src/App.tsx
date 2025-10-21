3eb03ec3111b07ecdcd016ccb4f1f974647142d8
mateus weinert <mateuswhj@gmail.com>
2025-10-21 14:38:13 -0300

    add icons and animationBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Box, createTheme, Divider, ThemeProvider, Typography } from '@mui/material'
+import { Box, createTheme, ThemeProvider, Typography } from '@mui/material'
 
 const theme = createTheme();
 
@@ -14,12 +14,19 @@ theme.typography.h3 = {
   },
 };
 
+const icons = [
+  "src/assets/icon_1.svg",
+  "src/assets/icon_2.svg",
+
+];
+
 function App() {
 
   return (
     <>
       <Header></Header>
-      <Divider sx={{ height: "2px", backgroundColor: "white" }}></Divider>
+      <AnimationBox></AnimationBox>
+
       <Box sx={{
         position: 'relative',
         width: '100%', height: 1000,
@@ -30,6 +37,7 @@ function App() {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
+        marginTop: 12
       }}>
         <InfoBox></InfoBox>
       </Box>
@@ -45,10 +53,11 @@ export function Header() {
         width: '100%',
         paddingLeft: 0,
         paddingRight: 0,
-        paddingTop: 0,
-        paddingBottom: 0,
+        paddingTop: 6,
+        paddingBottom: 6,
         display: 'flex',
         justifyContent: 'center',
+        backgroundColor: '#032C36'
       }} >
         <ThemeProvider theme={theme}>
           <Typography variant="h3">BIO NEWS</Typography>
@@ -69,9 +78,12 @@ export function InfoBox() {
           sx={{
             width: '100%',
             height: '100%',
-            backgroundColor: '#000000',
+            backgroundColor: '#A9C1C1',
+            color: 'black'
           }}
-        />
+        >
+          <p>Oie</p>
+        </Box>
       </Box>
     </>
   )
@@ -96,6 +108,36 @@ export function Tentacle2() {
       left: -52,
       zIndex: 1,
     }} />;
+}
+
+export function AnimationBox() {
+  return (
+    <>
+      <Box sx={{
+        width: '100%',
+        height: 24,
+        backgroundColor: '#032C36',
+        color: 'black',
+        display: "flex",
+        justifyContent: "center",
+      }}>
+        <Box sx={{
+          width: '90%',
+          height: 48,
+          backgroundColor: 'rgb(6 55 67)',
+          color: 'black'
+        }}>
+      {icons.map((src, i) => (
+              <img
+                key={i}
+                src={src}
+                className="icons"
+              />
+            ))}
+        </Box>
+      </Box>
+    </>
+  )
 }
 
 export default App
